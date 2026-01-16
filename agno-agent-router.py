cf601c555192de344fc878a087dd66cfe1855424
Ajinkya Temgire <ajinkya.temgire0835@gmail.com>
2026-01-16 15:51:13 +0530

    adding router example

---
+++ agno-agent-router.py
@@ -0,0 +1,9 @@
+from agno.agent import Agent
+from agno.models.groq import Groq 
+from agno.workflow import Step , Workflow , StepInput , Router
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
